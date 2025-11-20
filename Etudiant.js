8433ce0dde593fa17ddd754e1e50a9fbf2646c48
Meniar Mhir <meniar.mhir@cesjb.be>
2025-11-20 16:01:17 +0100

    essai3

--- Etudiant.js
+++ Etudiant.js
@@ -1,11 +1,14 @@
-class Etudiant{
+export default class Etudiant{
+
     nom 
     age 
     section 
 
-    constructor() {
-      this.nom=
-      this.age=
-      this.section=
+    constructor(nom,age,section) {
+      this.nom= nom
+      this.age= age
+      this.section= section
     }
+
+description (){ return'${this.nom}(${this.age} ans) - Section : ${this.section}';}
 }
---
+++ Main.js
@@ -0,0 +1,4 @@
+import Etudiant from "./etudiant";
+var user1= new Etudiant("meniar",19,"info")
+var user2= new Etudiant("lambda1",18,"science")
+var user3= new Etudiant("lambda2",18,"info")
